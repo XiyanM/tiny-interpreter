f0962d80833f41e97fd47955db344a8aa5929a5e
Xiyan Moo <xiy4nm@gmail.com>
2026-01-27 19:29:37 +0800

    Added TokenType and Token classes

---
+++ src/tokens.py
@@ -0,0 +1,38 @@
+from enum import Enum, auto
+
+class TokenType(Enum):
+    #Keywords
+    VAR = auto()
+    PRINT = auto()
+
+    #Literals
+    IDENTIFIER = auto()
+    STRING = auto()
+    NUMBER = auto()
+
+    #Single-Character Tokens
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+    COMMA = auto()
+    DOT = auto()
+    MINUS = auto()
+    PLUS = auto()
+    SEMICOLON = auto()
+    SLASH = auto()
+    STAR = auto()
+
+
+class Token:
+    def __init__(self, token_type: TokenType, lexeme: str, literal, line: int):
+        self.type = token_type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+    
+    def __repr__(self):
+        return f"Token({self.type.name}, {self.lexeme!r}, {self.literal!r})"
+
+        
+
